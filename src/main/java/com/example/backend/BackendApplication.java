04d9e3a10879b1d39b18c55a0bb33996096a85ac
jking412 <3250532948@qq.com>
2024-01-09 17:14:56 +0800

    feat: 打印系统中所有Pod

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,28 @@
 package com.example.backend;
 
-import org.springframework.boot.SpringApplication;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.Config;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		ApiClient client = Config.fromConfig("/home/jking/.kube/config");
+		Configuration.setDefaultApiClient(client);
+
+		CoreV1Api api = new CoreV1Api();
+		V1PodList list = api.listPodForAllNamespaces(null, null, null,
+				null, null, null, null,
+				null, null, null, null);
+		for (V1Pod item : list.getItems()) {
+			System.out.println(item.getMetadata().getName());
+		}
 	}
 
 }
