b68986d159224fe87bb9f3a15b1f2f71a81a6229
jking412 <3250532948@qq.com>
2024-02-12 21:57:50 +0800

    chore: 制作uos:v0.2.0

---
+++ docker/v0-2-0/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 设置vnc密码，默认为123456，如果存在环境变量VNC_PW，则使用环境变量的值
+if [ -z $VNC_PW ]; then
+    VNC_PW="123456"
+fi
+
+# 设置vnc密码
+echo $VNC_PW | vncpasswd -f > /root/.vnc/passwd
+
+# 启动vnc服务
+vncserver \
+    -localhost no \
+    -geometry 1024x768 \
+    -SecurityTypes VNCAUTH -passwd /root/.vnc/passwd \
+    :1
+
+# 启动websockify代理服务
+websockify --web /usr/share/novnc/ \
+    6080 \
+    localhost:5901
