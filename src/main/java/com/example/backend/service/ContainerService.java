fcedff55d260a86a04fffd4abf144f9354b6809e
jking412 <3250532948@qq.com>
2024-02-12 19:40:36 +0800

    feat: 添加containerService

--- src/main/java/com/example/backend/service/ContainerService.java
+++ src/main/java/com/example/backend/service/ContainerService.java
@@ -9,7 +9,21 @@ public class ContainerService {
     @Autowired
     UosService uosService;
 
-    public void create(String podName,String hostName ,int cpuReq,int cpuLimit,int memoryReq,int memoryLimit){
+    public static String uosImage = "uos";
 
+    public int create(String imageName,String podName,String hostName ,int cpuReq,int cpuLimit,int memoryReq,int memoryLimit)
+    throws Exception{
+        if (imageName.equals(uosImage)){
+            return uosService.create(podName,hostName,cpuReq,cpuLimit,memoryReq,memoryLimit);
+        }
+        return -1;
     }
+
+    public void delete(String imageName,int num) throws Exception{
+        if (imageName.equals(uosImage)){
+            uosService.delete(num);
+        }
+    }
+
+
 }
