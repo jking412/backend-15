4e13008a1b403632feced386016601d0580a424c
jking412 <3250532948@qq.com>
2024-02-14 16:05:09 +0800

    chore: 添加了与nfs相关的yaml文件
    该方案暂时被搁置，采用最简单的hostpath作为
    持久卷类型

---
+++ k3s-yaml/nfs/clean.sh
@@ -0,0 +1,7 @@
+kubectl delete -f mypv.yaml
+kubectl delete -f mypvc.yaml
+kubectl delete -f nfs-server-deployment.yaml
+kubectl delete -f nfs-server-service.yaml
+kubectl delete -f nfs-pv.yaml
+kubectl delete -f nfs-pvc.yaml
+kubectl delete -f uos.yaml
